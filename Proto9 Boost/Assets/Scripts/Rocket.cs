fd9ea0c8400aac185651fafbee5ac0aa68cef427
Pablo Nicoli <pablo.nicoli@hotmail.com>
2021-01-29 20:54:41 -0300

    Add movement with AWSD and rows with the enaught mass to hover

---
+++ Proto9 Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    public Rigidbody rocketRb;
+    public float thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space Bar precced");
+            rocketRb.AddRelativeForce(Vector3.up * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("moving left");
+            rocketRb.AddRelativeForce(Vector3.left * thrust);
+
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("moving Right");
+            rocketRb.AddRelativeForce(Vector3.right * thrust);
+        }
+    }
+}

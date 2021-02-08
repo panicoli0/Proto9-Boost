f61e2a4289c3a1cab1ff55ea1d8d555bb2bd89f3
panicoli0 <pablonicoli21@gmail.com>
2021-02-08 13:45:11 -0300

    Adding Level Manager

--- Proto9 Boost/Assets/Scripts/Rocket.cs
+++ Proto9 Boost/Assets/Scripts/Rocket.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -12,49 +13,75 @@ public class Rocket : MonoBehaviour
     
     AudioSource rocketSound;
 
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
+        
+
         rocketRb = GetComponent<Rigidbody>();
         rocketSound = GetComponent<AudioSource>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotate();
-        Thrust();
+        if (state == State.Alive)
+        {
+            Rotate();
+            Thrust();
+        }
+
+        
     }
 
     void OnCollisionEnter(Collision other)
     {
         
         {
+            if (state != State.Alive) { return; } //Ignora collisiones
+
             switch (other.gameObject.tag)
             {
+                case "OK":
+                    Debug.Log("Ganastes PAPA");
+                    break;
+
                 case "Friendly":
                     //Do nothing
                     Debug.Log("Este es: " + other.gameObject.tag);
                     break;
 
-                case "Fuel":
-                    //Carga de combustible
-                    Debug.Log("Este es: " + other.gameObject.tag);
-                    break;
-
-                case "OK":
+                case "Finish":
                     //Ganastes
+                    state = State.Transcending;
                     Debug.Log("Este es: " + other.gameObject.tag);
+                    Invoke("LoadNextLevel", 1f);
                     break;
 
                 default:
                     //Pediste
-                    Debug.Log("Este es: " + other.gameObject.tag);
+                    state = State.Dying;
+                    Debug.Log("Le pegaste a algo que te mata: " + other.gameObject.tag);
+                    Invoke("LoadFirstLevel", 1f);
                     break;
             }
         }
-        
+
+       
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Rotate()
